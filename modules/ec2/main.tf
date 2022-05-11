7128e3312c71094e2427e4f6c4f589f29b2b5961
Ami kalola <ami.kalola@solutionanalysts.in>
2022-05-11 18:58:00 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   subnet_id     = "${var.subnet_id}"
+  user_data = file("${var.script_path}/script.sh")
 
 #   tags {
 #     Name = "Hello-World"
